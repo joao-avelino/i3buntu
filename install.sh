4119fe1304f367cabe38300c4ac3abb395e8c7aa
João Avelino <avelino@DESKTOP-LENOVO.lan>
2018-03-24 21:47:29 +0000

    giving errors...

--- install.sh
+++ install.sh
@@ -111,60 +111,60 @@ apt-get install -y gir1.2-appindicator3-0.1 gdebi
 apt-get install -y caffeine
 
 ##### Get and install infinality (better font rendering)
-add-apt-repository ppa:no1wantdthisname/ppa -y
-apt-get update
-apt-get upgrade -y
-apt-get install -y fontconfig-infinality
+#add-apt-repository ppa:no1wantdthisname/ppa -y
+#apt-get update
+#apt-get upgrade -y
+#apt-get install -y fontconfig-infinality
 
 ###### Get and install playerctl
-wget 'https://github.com/acrisci/playerctl/releases/download/v0.5.0/playerctl-0.5.0_amd64.deb'
-dpkg -i playerctl-0.5.0_amd64.deb
-rm -rf playerctl-0.5.0_amd64.deb
+#wget 'https://github.com/acrisci/playerctl/releases/download/v0.5.0/playerctl-0.5.0_amd64.deb'
+#dpkg -i playerctl-0.5.0_amd64.deb
+#rm -rf playerctl-0.5.0_amd64.deb
 
 ###### Remove old versions of the Arc theme
-rm -rf /usr/share/themes/{Arc,Arc-Darker,Arc-Dark}
-rm -rf ~/.local/share/themes/{Arc,Arc-Darker,Arc-Dark}
-rm -rf ~/.themes/{Arc,Arc-Darker,Arc-Dark}
+#rm -rf /usr/share/themes/{Arc,Arc-Darker,Arc-Dark}
+#rm -rf ~/.local/share/themes/{Arc,Arc-Darker,Arc-Dark}
+#rm -rf ~/.themes/{Arc,Arc-Darker,Arc-Dark}
 
 ###### Get and install the latest Arc theme
-wget http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_16.04/Release.key
-apt-key add - < Release.key
-sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/arc-theme.list"
-apt-get update
-apt-get install -y arc-theme
+#wget http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_16.04/Release.key
+#apt-key add - < Release.key
+#sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/arc-theme.list"
+#apt-get update
+#apt-get install -y arc-theme
 
 ###### Make .fonts directory if not already available
-mkdir ~/.fonts
+#mkdir ~/.fonts
 
 ###### Get and install San Francisco Font
-git clone https://github.com/supermarin/YosemiteSanFranciscoFont.git
-cp -v YosemiteSanFranciscoFont/*.ttf ~/.fonts
-rm -rf YosemiteSanFranciscoFont
+#git clone https://github.com/supermarin/YosemiteSanFranciscoFont.git
+#cp -v YosemiteSanFranciscoFont/*.ttf ~/.fonts
+#rm -rf YosemiteSanFranciscoFont
 
 ###### Get and install Font Awesome Web Font
-git clone https://github.com/FortAwesome/Font-Awesome.git
-cp -v Font-Awesome/fonts/*.ttf ~/.fonts
-rm -rf Font-Awesome
+#git clone https://github.com/FortAwesome/Font-Awesome.git
+#cp -v Font-Awesome/fonts/*.ttf ~/.fonts
+#rm -rf Font-Awesome
 
 ###### Get and install Moka icon theme
-add-apt-repository ppa:moka/daily -y
-apt-get update
-apt-get install -y moka-icon-theme
+#add-apt-repository ppa:moka/daily -y
+#apt-get update
+#apt-get install -y moka-icon-theme
 
 ###### Make config directories
-mkdir ~/.config
-mkdir ~/.config/gtk-3.0
-mkdir ~/.config/i3
+#mkdir ~/.config
+#mkdir ~/.config/gtk-3.0
+#mkdir ~/.config/i3
 
 ###### Apply GTK theme, fonts, icon theme, login greeter
 ###### and i3
-cp -f ~/i3buntu-master/configs/gtk/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
-cp -f ~/i3buntu-master/configs/gtk/.gtkrc-2.0 ~/.gtkrc-2.0
-cp -f ~/i3buntu-master/configs/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
-cp -f ~/i3buntu-master/configs/i3/config ~/.config/i3/config
+#cp -f ~/i3buntu-master/configs/gtk/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
+#cp -f ~/i3buntu-master/configs/gtk/.gtkrc-2.0 ~/.gtkrc-2.0
+#cp -f ~/i3buntu-master/configs/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
+#cp -f ~/i3buntu-master/configs/i3/config ~/.config/i3/config
 
 ###### Set appropriate user permissions
-chown $(whoami):$(whoami) -R /home/$(whoami)/
-chmod g+s /home/$(whoami)/
-setfacl -d -m g::rwx /home/$(whoami)/
-setfacl -d -m o::rx /home/$(whoami)/
+#chown $(whoami):$(whoami) -R /home/$(whoami)/
+#chmod g+s /home/$(whoami)/
+#setfacl -d -m g::rwx /home/$(whoami)/
+#setfacl -d -m o::rx /home/$(whoami)/
